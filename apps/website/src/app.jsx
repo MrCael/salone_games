322dd107547e2d01c00d7c6ac6ef0e710381d4ec
Cael Erickson <cse37@byu.edu>
2025-12-03 14:46:29 -0700

    Installed bootstrap and displayed placeholder logo and sign in button

--- apps/website/src/app.jsx
+++ apps/website/src/app.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 import { BrowserRouter, NavLink, Route, Routes, useLocation } from "react-router-dom";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./app.css";
 
 export default function App() {
     return (
         <BrowserRouter>
             <div>
-                <header>
-                    <nav>
-                        <menu></menu>
-                    </nav>
+                <header className="d-flex flex-row">
+                    <img className="main-logo" src="logoipsum-242.png" alt="logo" />
+                    <div className="sign-in-button"><i className="bi bi-box-arrow-in-right"></i>Sign In</div>
                 </header>
                 <Routes></Routes>
                 <footer></footer>
