6df2b1c031444da820192d4d84d692ef0256c633
Cael Erickson <cse37@byu.edu>
2025-12-04 18:59:42 -0700

    Fixed nav bar layout

--- apps/website/src/app.jsx
+++ apps/website/src/app.jsx
@@ -1,21 +1,21 @@
-import React from "react"
+import React from "react";
+import Button from "react-bootstrap/Button";
+
 import { BrowserRouter, NavLink, Route, Routes, useLocation } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import './custom.scss';
 import "./app.css";
 
 export default function App() {
     return (
         <BrowserRouter>
-            <div>
-                <header className="d-flex flex-row">
-                    <img className="main-logo" src="logoipsum-242.png" alt="logo" />
-                    <div className="sign-in-button"><i className="bi bi-box-arrow-in-right"></i>Sign In</div>
-                </header>
-                <Routes></Routes>
-                <footer></footer>
-            </div>
+            <header>
+                <img className="main-logo" src="logoipsum-242.png" alt="logo" />
+                <Button className="sign-in-button float-end"><i className="bi bi-box-arrow-in-right"></i>&nbsp;Sign In</Button>
+            </header>
+            <Routes></Routes>
+            <footer></footer>
         </BrowserRouter>
     )
 }
---
+++ notes.md
@@ -0,0 +1,13 @@
+This file contains my personal notes about this project and anything I learned that I want to remember.
+
+# Sass
+
+To override Bootstrap Sass variables, first remove the Bootstrap import from your main `.js` or `.jsx` file. Then create a custom `.scss` file and format it like the following code block:
+
+```
+@use "bootstrap/scss/bootstrap" with (
+    $primary: #0072c6
+);
+```
+
+Then import that file in your main `.js` or `.jsx` file. Normally `.scss` files need to be compiled in to CSS before they are used, but Vite will do that for me automatically.
