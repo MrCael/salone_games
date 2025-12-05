d6674912428d85029185c87ada0fc05d5410a810
Cael Erickson <cse37@byu.edu>
2025-12-05 16:25:32 -0700

    Created about page with some basic desciption of project and inspiration and allowed navigation between empty home page and about page

--- apps/website/src/app.jsx
+++ apps/website/src/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 
 import { BrowserRouter, NavLink, Route, Routes, useLocation } from "react-router-dom";
+import { About } from "./about/about";
 
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './custom.scss';
@@ -11,10 +12,16 @@ export default function App() {
     return (
         <BrowserRouter>
             <header>
-                <img className="main-logo" src="logoipsum-242.png" alt="logo" />
-                <Button className="sign-in-button float-end"><i className="bi bi-box-arrow-in-right"></i>&nbsp;Sign In</Button>
+                <NavLink to="/">
+                    <img className="main-logo" src="logoipsum-242.png" alt="logo" />
+                </NavLink>
+                <Button className="float-end menu-button"><i className="bi bi-box-arrow-in-right"></i>&nbsp;Sign In</Button>
+                <NavLink className="btn btn-primary float-end menu-button" to="/about"><i class="bi bi-people"></i>&nbsp;About</NavLink>
             </header>
-            <Routes></Routes>
+            <Routes>
+                <Route path="/" element={<></>} exact />
+                <Route path="/about" element={<About />} />
+            </Routes>
             <footer></footer>
         </BrowserRouter>
     )
