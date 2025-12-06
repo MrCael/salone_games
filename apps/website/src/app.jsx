18df37cbdc164271254d97dd993fe1e906e3adae
Cael Erickson <cse37@byu.edu>
2025-12-06 15:17:53 -0700

    Created login page and boilerplate code for page to create user

--- apps/website/src/app.jsx
+++ apps/website/src/app.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 
-import { BrowserRouter, NavLink, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import { About } from "./about/about";
+import { Login } from "./login/login";
+import { CreateUser } from "./user/create/create";
 
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './custom.scss';
@@ -15,12 +17,14 @@ export default function App() {
                 <NavLink to="/">
                     <img className="main-logo" src="logoipsum-242.png" alt="logo" />
                 </NavLink>
-                <Button className="float-end menu-button"><i className="bi bi-box-arrow-in-right"></i>&nbsp;Sign In</Button>
+                <NavLink className="btn btn-primary float-end menu-button" to="/login"><i className="bi bi-box-arrow-in-right"></i>&nbsp;Sign In</NavLink>
                 <NavLink className="btn btn-primary float-end menu-button" to="/about"><i className="bi bi-people"></i>&nbsp;About</NavLink>
             </header>
             <Routes>
                 <Route path="/" element={<></>} exact />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/create" element={<CreateUser />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
             <footer></footer>
         </BrowserRouter>
---
+++ apps/website/src/login/login.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+import { NavLink } from "react-router-dom";
+
+export function Login() {
+    return (
+        <main className="d-flex flex-grow-1 justify-content-center align-items-center">
+            <div className="card w-auto h-auto login-card">
+                <div className="card-body">
+                    <p>Username:</p>
+                    <input type="text" className="form-control" style={{ width: "300px" }} />
+                    <p>Password:</p>
+                    <input type="password" className="form-control" />
+                    <Button className="form-control margin-top-10">Sign In</Button>
+                    <p className="margin-top-10">Don't have an account? Create one <NavLink to="/user/create">here!</NavLink></p>
+                </div>
+            </div>
+        </main>
+    );
+}
